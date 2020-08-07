c974c473e171fe2960ee14abac382ca280516e2e
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2020-08-06 20:40:47 -0500

    Application Startup and Configuration

---
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using OdeToFood.Data.Services;
+
+namespace OdeToFood.Web
+{
+    public class ContainerConfig
+    {
+        public static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<InMemoryRestaurantData>()
+                .As<IRestaurantData>()
+                .SingleInstance();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
